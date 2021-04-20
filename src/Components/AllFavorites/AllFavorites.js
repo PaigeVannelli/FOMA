38d92a46116265ed8985397adb4bf38ea32794f6
Paige Vannelli <paigevannelli@gmail.com>
2021-04-20 14:08:17 -0700

    Create Component files

---
+++ src/Components/AllFavorites/AllFavorites.js
@@ -0,0 +1,4 @@
+import './AllFavorites.css'
+
+// Function component 
+// should get and map through all favorited art images and titles
---
+++ src/Components/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <h1>TEST</h1>
+    )
+  }
+}
+
+export default App;
