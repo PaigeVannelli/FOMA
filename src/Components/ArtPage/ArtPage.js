71affa905ae358471305fed552c754b77e58de35
Paige Vannelli <paigevannelli@gmail.com>
2021-04-21 14:07:50 -0700

    Create data cleaning function

--- src/Components/ArtPage/ArtPage.js
+++ src/Components/ArtPage/ArtPage.js
@@ -1,5 +1,6 @@
 import './ArtPage.css'
 import React, { Component } from 'react'
+import ArtDetails from '../artDetails/ArtDetails'
 
 class ArtPage extends Component {
   constructor(props) {
@@ -10,18 +11,33 @@ class ArtPage extends Component {
     }
   }
 
+  simplifyArtObject = (artObject) => {
+    let cleanedArtObject = {}
+    cleanedArtObject.id = artObject.objectID
+    cleanedArtObject.title = artObject.title
+    cleanedArtObject.artist = artObject.artistDisplayName
+    cleanedArtObject.medium = artObject.medium
+    cleanedArtObject.date = artObject.objectDate
+    cleanedArtObject.image = artObject.primaryImage
+    cleanedArtObject.smallImage = artObject.primarySmallImage
+    return cleanedArtObject
+  }
+
   componentDidMount = () => {
     fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${this.props.currentArtID}`)
       .then(response => response.json())
+      .then(artObject => this.simplifyArtObject(artObject))
       .then(data => this.setState({ currentArt: data }))
   }
 
   render = () => {
     return (
-      <article>
-        <h1>Test</h1>
+      <section>
+        {/* <ArtImage /> do I need this? */}
         <img src={this.state.currentArt.primaryImage} />
-      </article>
+        {/* <ArtDetails /> */}
+        <h1>Test</h1>
+      </section>
     )
   }
 }
