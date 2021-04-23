3a0f9980bdfe8405395503ed1c64b3afff151ea7
Paige Vannelli <paigevannelli@gmail.com>
2021-04-22 21:21:58 -0700

    Delete commented out lines

--- src/Components/ArtPage/ArtPage.js
+++ src/Components/ArtPage/ArtPage.js
@@ -4,41 +4,25 @@ import ArtDetails from '../artDetails/ArtDetails'
 import fetchArtInfo from '../../ApiCalls'
 
 const ArtPage = (props) => {
-
-  // simplifyArtObject = (artObject) => {
-  //   let cleanedArtObject = {}
-  //   cleanedArtObject.id = artObject.objectID
-  //   cleanedArtObject.title = artObject.title
-  //   cleanedArtObject.artist = artObject.artistDisplayName
-  //   cleanedArtObject.medium = artObject.medium
-  //   cleanedArtObject.date = artObject.objectDate
-  //   cleanedArtObject.image = artObject.primaryImage
-  //   cleanedArtObject.smallImage = artObject.primarySmallImage
-  //   return cleanedArtObject
-  // }
-
-
-  // render = () => {
-    return (
-      <section className='art-page'>
-        {
-          props.currentArtID ?
-        <>
-          <div className='art-piece-container'>
-            <img src={props.currentArt.image} alt={props.currentArt.title} className='art-piece'/>
-          </div>
-          <div className='display-next-button-container'>
-            <button className='display-next-button' onClick={props.displayNextPiece}>></button>
-          </div>
-          <ArtDetails currentArt={props.currentArt} addFavorite={props.addFavorite}/>
-        </>
-        :
-        <h1>loading</h1>
-        }
-        {/* <ArtImage /> do I need this? */}
-      </section>
-    )
-  }
-// }
+  return (
+    <section className='art-page'>
+      {
+        props.currentArtID ?
+      <>
+        <div className='art-piece-container'>
+          <img src={props.currentArt.image} alt={props.currentArt.title} className='art-piece'/>
+        </div>
+        <div className='display-next-button-container'>
+          <button className='display-next-button' onClick={props.displayNextPiece}>></button>
+        </div>
+        <ArtDetails currentArt={props.currentArt} addFavorite={props.addFavorite}/>
+      </>
+      :
+      <h1>loading</h1>
+      }
+      {/* <ArtImage /> do I need this? */}
+    </section>
+  )
+}
 
 export default ArtPage
