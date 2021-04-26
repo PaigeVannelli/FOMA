76ce65984dd8c6c6eeaa22b5e6e792c3c912130c
Paige Vannelli <paigevannelli@gmail.com>
2021-04-25 21:19:04 -0700

    Clean up AllFavorites component

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,7 +1,6 @@
 import './Search.css'
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import search from '../../assets/search.svg'
 import PropTypes from 'prop-types'
 
 class Search extends Component {
