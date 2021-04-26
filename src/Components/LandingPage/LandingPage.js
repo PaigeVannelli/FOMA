fe0aecbf6f910e7f4bc2b95a245834a2bbe0ad4b
Paige Vannelli <paigevannelli@gmail.com>
2021-04-25 19:57:48 -0700

    Add cursers to buttons

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -8,9 +8,7 @@ const LandingPage = ({ search }) => {
       <div className='main-text'>
         <h1 className='main-title'>FOMA</h1>
         <h2 className='subtitle'>Fear of Missing Art</h2>
-        <h2 className='dedication'>Museum of Modern Art</h2>
-        {/* <h3 className='dedication'>Dedicated to Moderna and Pfizer</h3> */}
-        {/* <h3 className='dedication'>Courtesy of the Met Gallary</h3> */}
+        <h2 className='dedication'>Art Gallery</h2>
         <Search search={search}/>
       </div>
     </section>
