433d3c0f07e8e88841fa075d4967c3aa9e7d62a7
Paige Vannelli <paigevannelli@gmail.com>
2021-04-25 18:47:00 -0700

    Change favorites logo

--- src/Components/nav/Nav.js
+++ src/Components/nav/Nav.js
@@ -1,40 +1,13 @@
 import './Nav.css'
 import { Link, useLocation } from 'react-router-dom'
 import home from '../../assets/home.svg'
-import favorites from '../../assets/heart.svg'
+import bookmark from '../../assets/bookmark.svg'
 import search from '../../assets/search.svg'
 import PropTypes from 'prop-types'
 import logo from '../../assets/logo.png'
 
 const Nav = ({ resetSearch }) => {
   return (
-    // <nav className='button-nav'>
-    //   <Link to='/'>
-    //     <button data-cy='home-button' onClick={resetSearch}>
-    //       <img src={home} className='home button' alt='home-button'/>
-    //     </button>
-    //   </Link>
-    //   {
-    //     useLocation().pathname === '/favorites' ?
-    //     <Link to='/gallery'>
-    //       <img 
-    //         data-cy='search-button' 
-    //         src={search} 
-    //         className='button' 
-    //         alt='search-button'
-    //       />
-    //     </Link>
-    //     :
-    //     <Link to='/favorites'>
-    //       <img 
-    //         data-cy='view-favorites' 
-    //         src={favorites} 
-    //         className='favorites button' 
-    //         alt='favorites-button'
-    //       />
-    //     </Link>
-    //   }
-    // </nav>
     <nav className='header'>
       <img className='logo' src={logo} />
       <div className='button-nav'>
@@ -57,7 +30,7 @@ const Nav = ({ resetSearch }) => {
         <Link className='link' to='/favorites'>
           <img 
             data-cy='view-favorites' 
-            src={favorites} 
+            src={bookmark} 
             className={`button ${useLocation().pathname === '/favorites' && 'favorites-highlighted'}`}
             alt='favorites-button'
           />
